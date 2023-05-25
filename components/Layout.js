90bba25d940c3dd6fb45fad2f0efb30c83d9c6c9
jitendra <jitendra.s1487@gmail.com>
2023-05-25 11:52:41 +0530

    content change

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,8 @@ export default function Layout({ children }) {
         <Link href="/">
           <a>
             <h1>
-            <span>Just Add Your Favrorite</span>
-              <span>Food Recipe</span>
+            <span>North Indian</span>
+              <span>Food Recipes</span>
             </h1>
             <h2>Spread The Joy</h2>
           </a>
